0289d0fe1f592e5610e40b9ad9896137aafc048a
huutien3112 <63644043+huutien3112@users.noreply.github.com>
2020-08-06 03:15:27 +0700

    commit

--- js.js
+++ js.js
@@ -69,90 +69,153 @@ function clickCounter() {
     }
   }
 
-console.log("alo");
 
 
 
-    // Định nghĩa một mảng các phần tử sẽ bỏ vào giỏ hàng
-    var shoppingCartItems = [];
+    // cart 2
+    let carts = document.querySelectorAll('.add-cart');
+    let products = [
+      {
+        name: 'Tivi 1',
+        tag: 'Television 1',
+        price: 15,
+        inCart: 0
+      },
+      {
+        name: 'Tivi 2',
+        tag: 'Television 2',
+        price: 20,
+        inCart: 0
+      },
+      {
+        name: 'Tivi 3',
+        tag: 'Television 3',
+        price: 15,
+        inCart: 0
+      },
+      {
+        name: 'Tivi 4',
+        tag: 'Television 4',
+        price: 20,
+        inCart: 0
+      },
+      
+    ];
 
-    $(document).ready(function () {
-        // Kiểm tra nếu đã có sessionStorage["shopping-cart-items"] hay chưa?
-        if (sessionStorage["shopping-cart-items"] != null) {
-            shoppingCartItems = JSON.parse(sessionStorage["shopping-cart-items"].toString());
-        }
-
-        // Hiển thị thông tin từ giỏ hàng
-        displayShoppingCartItems();
-    });
-
-
-    // Sự kiện click các button có class=".add-to-cart"
-    $(".add-to-cart").click(function () {
-        var button = $(this); // Lấy đối tượng button mà người dùng click
-        var id = button.attr("id"); // id của sản phẩm là id của button
-        var name = button.attr("data-name"); // name của sản phẩm là thuộc tính data-name của button
-        var price = button.attr("data-price"); // price của sản phẩm là thuộc tính data-price của button
-        var quantity = 1; // Số lượng
-
-
-        var item = {
-            id: id,
-            name: name,
-            price: price,
-            quantity: quantity
-        };
-
-        var exists = false;
-        if (shoppingCartItems.length > 0) {
-            $.each(shoppingCartItems, function (index, value) {
-                // Nếu mặt hàng đã tồn tại trong giỏ hàng thì chỉ cần tăng số lượng mặt hàng đó trong giỏ hàng.
-                if (value.id == item.id) {
-                    value.quantity++;
-                    exists = true;
-                    return false;
-                }
-            });
-        }
-
-        // Nếu mặt hàng chưa tồn tại trong giỏ hàng thì bổ sung vào mảng
-        if (!exists) {
-            shoppingCartItems.push(item);
-        }
-
-        // Lưu thông tin vào sessionStorage
-        sessionStorage["shopping-cart-items"] = JSON.stringify(shoppingCartItems); // Chuyển thông tin mảng shoppingCartItems sang JSON trước khi lưu vào sessionStorage
-        // Gọi hàm hiển thị giỏ hàng
-        displayShoppingCartItems();
-    });
-
-    // Xóa hết giỏ hàng shoppingCartItems
-    $("#button-clear").click(function () {
-        shoppingCartItems = [];
-        sessionStorage["shopping-cart-items"] = JSON.stringify(shoppingCartItems);
-        $("#table-products > tbody").html("");
-    });
-
-
-    // Hiển thị giỏ hàng ra table
-    function displayShoppingCartItems() {
-        if (sessionStorage["shopping-cart-items"] != null) {
-            shoppingCartItems = JSON.parse(sessionStorage["shopping-cart-items"].toString()); // Chuyển thông tin từ JSON trong sessionStorage sang mảng shoppingCartItems.
-
-            $("#table-products > tbody").html("");
-            // Duyệt qua mảng shoppingCartItems để append từng item dòng vào table
-            $.each(shoppingCartItems, function (index, item) {
-                var htmlString = "";
-                htmlString += "<tr>";
-                htmlString += "<td>" + item.id + "</td>";
-                htmlString += "<td>" + item.name + "</td>";
-                htmlString += "<td style='text-align: right'>" + item.price + "</td>";
-                htmlString += "<td style='text-align: right'>" + item.quantity + "</td>";
-                htmlString += "<td style='text-align: right'>" + item.price * item.quantity + "</td>";
-                htmlString += "</tr>";
-
-                $("#table-products > tbody:last").append(htmlString);
-
-            });
-        }
+    for (let i=0; i < carts.length; i++) {
+        carts[i].addEventListener('click', () => {
+            cartNumbers(products[i]);
+            totalCost(products[i])
+        })
     }
+
+    function onLoadCartNumbers(){
+      let productNumbers = localStorage.getItem('cartNumbers');
+
+      if (productNumbers){
+        document.querySelector('.cart span').textContent = productNumbers;
+      }
+    }
+
+
+    
+    function cartNumbers(product){
+
+      let productNumbers = localStorage.getItem('cartNumbers');
+
+      productNumbers = parseInt(productNumbers);
+
+      if (productNumbers){
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+        document.querySelector('.cart span').textContent = productNumbers + 1;
+      } else{
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelector('.cart span').textContent = 1;
+      }
+
+      setItems(product);
+    }
+
+    function setItems(product){
+      let cartItems = localStorage.getItem('productsInCart');
+      cartItems = JSON.parse(cartItems);
+
+      if (cartItems != null){
+        
+        if(cartItems[product.tag] == undefined){
+          cartItems = {
+            ...cartItems,
+            [product.tag]: product
+          }
+        }
+        cartItems[product.tag].inCart += 1;
+      } else{
+        product.inCart = 1;
+        cartItems = {
+          [product.tag]: product
+      }
+      }
+
+      localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+    }
+
+    function totalCost(product){
+      // console.log("the price", product.price);
+      let cartCost = localStorage.getItem('totalCost');
+      
+
+      console.log("my cost is", cartCost);
+      console.log(typeof cartCost);
+
+      if (cartCost != null){
+        cartCost = parseInt(cartCost);
+        localStorage.setItem("totalCost", cartCost + product.price);
+      }else{
+        localStorage.setItem("totalCost", product.price);
+      }
+    }
+
+    function displayCart(){
+      let cartItems = localStorage.getItem("productsInCart")
+      cartItems = JSON.parse(cartItems);
+      let productContainer = document.querySelector(".products");
+      let cartCost = localStorage.getItem('totalCost');
+
+
+      console.log(cartItems);
+      if(cartItems && productContainer){
+        productContainer.innerHTML = '';
+        Object.values(cartItems).map(item => {
+          productContainer.innerHTML += `
+          <div class="product">
+            <ion-icon name="close-circle-outline"></ion-icon>
+            <img src="./images/${item.tag}.jpg">
+            <span>${item.name}</span>
+          </div>  
+          <div class="price">
+            $${item.price},00
+          </div>
+          <div class="quantity">
+            <ion-icon name="arrow-back-circle-outline"></ion-icon>
+            <span>${item.inCart}</span>
+            <ion-icon name="arrow-forward-circle-outline"></ion-icon>
+          </div>
+          <div class="total">
+            $${item.inCart * item.price},00
+          </div>
+          `
+        });
+        productContainer.innerHTML += `
+          <div class="basketTotalContainer">
+            <h4 class="basketTotalTitle">
+              Basket Total
+            </h4>
+            <h4 class="basketTotal">
+              $${cartCost},00
+            </h4>
+        `;
+      }
+    }
+
+    onLoadCartNumbers();
+    displayCart();
